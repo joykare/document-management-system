245a49aa436edadd9a52431693fe2c8143cf7d8a
andela-jwarugu <joy.warugu@andela.com>
2016-09-08 14:24:09 +0300

    Add role test file

---
+++ tests/role.js
@@ -0,0 +1,9 @@
+var app = require('../index');
+var request = require('supertest')(app);
+var expect = require('chai').expect;
+var Role = require('../server/models/role.js');
+
+describe('Role test suite' function(){
+  var token;
+  expect(true).to.equal(true);
+}
