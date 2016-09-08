11132d975972f5b968798efeae7b3db0495bde8e
andela-jwarugu <joy.warugu@andela.com>
2016-09-08 10:32:03 +0300

    Add methods to add user roles

--- server/controllers/role_controller.js
+++ server/controllers/role_controller.js
@@ -14,15 +14,22 @@ module.exports = {
   create: function(req, res){
     var possibleRoles = Role.schema.path('title').enumValues;
     console.log(Role.schema.path('title').enumValues);
-    // var role = new Role();
-    // role.title = req.body.role;
-    // role.save(function(err){
-    //   if (err){
-    //     res.send(err);
-    //   }
-    //   else {
-    //     res.send({message:'Role saved'});
-    //   }
-    // });
+
+    var role = new Role();
+
+    if (possibleRoles.indexOf(req.body.role) !== -1) {
+        role.title = req.body.role;
+    } else {
+      res.status(400).send({message: 'Is not an allowable role'});
+    }
+
+    role.save(function(err){
+      if (err){
+        res.send(err);
+      }
+      else {
+        res.send({message:'Role saved'});
+      }
+    });
   }
 }
--- server/routes/role_route.js
+++ server/routes/role_route.js
@@ -3,5 +3,6 @@ var Role = require('../controllers/role_controller');
 module.exports = function(router){
 
   router.route('/roles')
+    .get(Role.get)
     .post(Role.create);
 }
