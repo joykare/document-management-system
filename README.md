a32d1fb178db6940dbdfa20d11a382c501034d7c
andela-jwarugu <joy.warugu@andela.com>
2016-09-08 15:04:56 +0300

    Add coveralls configuratons

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Document-Management-System
 Checkpoint 3
+
+[![Coverage Status](https://coveralls.io/repos/github/andela-jwarugu/Document-Management-System/badge.svg?branch=develop)](https://coveralls.io/github/andela-jwarugu/Document-Management-System?branch=develop)
--- tests/role.js
+++ tests/role.js
@@ -3,7 +3,7 @@ var request = require('supertest')(app);
 var expect = require('chai').expect;
 var Role = require('../server/models/role.js');
 
-describe('Role test suite' function(){
+describe('Role test suite', function(){
   var token;
   expect(true).to.equal(true);
-}
+});
